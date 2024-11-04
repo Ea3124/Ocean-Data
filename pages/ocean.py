d8ca15d8a5fa2ff5062f453d6d446e4304821ce4
Ea3124 <leesj6717@naver.com>
2024-11-05 03:48:35 +0900

    feat: geolocation api 구현 & flask 서버 https프로토콜 구현

--- pages/ocean.py
+++ pages/ocean.py
@@ -1,6 +1,33 @@
+# ocean.py
 import streamlit as st
+import requests
 
 def show():
     st.title("Ocean Page")
-    st.write("This is the Ocean page dedicated to ocean-related data and insights.")
-    st.write("Detailed ocean-related information will be displayed here.")
+
+    # 클라이언트의 IP 주소 가져오기
+    # try:
+    #     ip_response = requests.get('https://api.ipify.org?format=json')
+    #     ip_address = ip_response.json().get('ip')
+    #     st.write(f"클라이언트 IP 주소: {ip_address}")
+
+    #     # Google Geolocation API를 사용하여 위치 정보 가져오기
+    #     LOCATION_API_KEY = "localkey"  # Streamlit Secrets 사용 하거나 해야함
+    #     geolocation_url = f'https://www.googleapis.com/geolocation/v1/geolocate?key={LOCATION_API_KEY}'
+    #     geolocation_data = {'considerIp': True}
+    #     geo_response = requests.post(geolocation_url, json=geolocation_data)
+    #     geo_result = geo_response.json()
+
+    #     if 'location' in geo_result:
+    #         latitude = geo_result['location']['lat']
+    #         longitude = geo_result['location']['lng']
+    #         st.write(f"추정 위치는 위도 {latitude}, 경도 {longitude}입니다.")
+    #     else:
+    #         st.write("위치 정보를 가져올 수 없습니다.")
+    # except Exception as e:
+    #     st.write("위치 정보를 가져오는 중 오류가 발생했습니다.")
+    #     st.write(str(e))
+
+    latitude = 35.241984
+    longitude = 129.0797056
+    st.write(f"추정 위치는 위도 {latitude}, 경도 {longitude}입니다.")
--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, jsonify
+from pyngrok import conf, ngrok
 
 app = Flask(__name__)
+
+http_tunnel = ngrok.connect(5000)
+tunnels = ngrok.get_tunnels()
+
+for kk in tunnels :
+    print(kk)
 
 @app.route('/', methods=['GET'])
 def home():
@@ -19,5 +26,17 @@ def get_data():
     }
     return jsonify(data)
 
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=5000, debug=True)  # 모든 네트워크 인터페이스에서 접근 가능하게 설정
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)  # 모든 네트워크 인터페이스에서 접근 가능하게 설정
+    # ngrok 터널 연결 (스크립트가 직접 실행될 때만)
+    port = 5000
+    public_url = ngrok.connect(port).public_url
+    print(f" * ngrok 터널 URL: {public_url}")
+
+    # 애플리케이션 컨텍스트에 public_url 설정 (필요한 경우)
+    app.config["public_url"] = public_url
+
+    # Flask 앱 실행 (use_reloader=False 설정)
+    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=False)
